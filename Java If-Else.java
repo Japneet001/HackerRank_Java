bce765cb6cd380829e02312ab5820a91e3051795
Japneet Kaur <jkaur01022004@gmail.com>
2023-07-12 21:11:55 +0530

    updates

--- Java If-Else.java
+++ Java If-Else.java
@@ -27,8 +27,6 @@ import java.util.regex.*;
 
 public class Solution {
 
-
-
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -36,20 +34,16 @@ public class Solution {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         scanner.close();
-            if (N%2!=0){
+        if (N % 2 != 0) {
             System.out.print("Weird");
+        } else {
+            if (2 <= N && N <= 5) {
+                System.out.print("Not Weird");
+            } else if (6 <= N && N <= 20) {
+                System.out.print("Weird");
+            } else {
+                System.out.print("Not Weird");
             }
-            else{
-                if(2<=N && N<=5){
-                    System.out.print("Not Weird");
-                }
-                else if (6<=N && N<=20){
-                    System.out.print("Weird");
-                }
-                else{
-                    System.out.print("Not Weird");
-                }
-            }
+        }
     }
 }
-
